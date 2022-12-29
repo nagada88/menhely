8a9d1a7c655335d82f3ea917bf82f0ae67e88323
Adam Nagy <nagada88@gmail.com>
2022-12-29 23:30:32 +0100

    bemutatgyorsit

--- app_menhely/models.py
+++ app_menhely/models.py
@@ -195,6 +195,45 @@ class Bemutatkozas(TranslatableModel):
     tartalom = models.TextField(),)
     bemutatkozas_main_img = models.ImageField(upload_to='app_menhely/img/photos')
     priority = models.IntegerField(default=5, validators=[MaxValueValidator(100), MinValueValidator(1)] )
-    
+
+    def save(self, *args, **kwargs):
+        if not self.bemutatkozas_main_img.closed:
+            if not self.make_thumbnail():
+                # set to a default thumbnail
+                raise Exception('Could not create thumbnail - is the file type valid?')
+
+        super(Bemutatkozas, self).save(*args, **kwargs)
+
+
+    def make_thumbnail(self):
+
+        image = Image.open(self.bemutatkozas_main_img)
+        image.thumbnail((1000,1000), Image.ANTIALIAS)
+
+        thumb_name, thumb_extension = os.path.splitext(self.bemutatkozas_main_img.name)
+        thumb_extension = thumb_extension.lower()
+
+        thumb_filename = thumb_name + '_thumb' + thumb_extension
+
+        if thumb_extension in ['.jpg', '.jpeg']:
+            FTYPE = 'JPEG'
+        elif thumb_extension == '.gif':
+            FTYPE = 'GIF'
+        elif thumb_extension == '.png':
+            FTYPE = 'PNG'
+        else:
+            return False  # Unrecognized file type
+
+        # Save thumbnail to in-memory file as StringIO
+        temp_thumb = BytesIO()
+        image.save(temp_thumb, FTYPE)
+        temp_thumb.seek(0)
+
+        # set save=False, otherwise it will run in an infinite loop
+        self.bemutatkozas_main_img.save(thumb_filename, ContentFile(temp_thumb.read()), save=False)
+        temp_thumb.close()
+
+        return True
+        
     def __str__(self):
         return self.cim
