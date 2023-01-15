aa3d394557dbc8922fe3ce5c204762e8f1b246f4
Adam Nagy <nagada88@gmail.com>
2023-01-15 19:50:40 +0100

    google analytics

--- allatmenhely/settings.py
+++ allatmenhely/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
+ALLOWED_HOSTS = ['www.soltimenhely.hu', '.soltimenhely.hu']
 
 
 # Application definition
